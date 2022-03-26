894fcab079c90d4f0c43b0fc666147f61ead8255
Arthur-Gaudium <93216989+Arthur-Gaudium@users.noreply.github.com>
2022-03-26 00:03:45 -0300

    #5 - Updating DML for NPCs

--- docs/SQL/DML-BD.sql
+++ docs/SQL/DML-BD.sql
@@ -3,20 +3,20 @@
 INSERT INTO elemento (nome) VALUES ('fogo'), ('água'), ('grama'), ('voador'), ('lutador'), ('veneno'), ('elétrico'), ('terra'), ('pedra'), ('psiquico'), ('gelo'), ('inseto'), ('fantasma'), ('ferro'), ('dragão'), ('sombrio'), ('fada'), ('normal');
 
 -- INSERT group 2: NPC, TREINADOR, MOCHILA, POKEDEX
--- TODO: Quando o mapa for criado, corrigir NPCs e Professor
+-- TODO: Quando o mapa for criado, corrigir NPCs e Professor - DONE, mas testar com mapa ainda sim
 
 -- Treinador
 INSERT INTO treinador (nome, nivel, dinheiro, insignia, id_posicao, id_professor) VALUES ('Ash Ketchum', 0, 0.00, 'iniciante', 1, 1);
 
 -- Professor
 INSERT INTO npc (nome, fala, profissao, id_posicao) VALUES 
-('Professor Oak','Olá treinador!! Bem vindo a sua jornada, escolha um pokemon e comece sua aventura!', 'professor', 0),
-('Vendedor June','Bem vindo a minha lojinha! Fique avontade!!', 'vendedor', 0),
-('Vendedora May','Ei muleque, não toque em nada que não vá pagar!!', 'vendedor', 0),
-('Vendedor July','Olá treinador!! Bem vindo a sua jornada, escolha um pokemon e comece sua aventura!', 'vendedor', 0),
-('Vendedora April','Olá treinador!! Bem vindo a sua jornada, escolha um pokemon e comece sua aventura!', 'vendedor', 0),
-('Aventureiro Jonas','Fazem alguns meses desde que sai da minha cidade natal para minha jornada, é um pouco solitário mas é uma experiência incrível', 'aventureiro', 0),
-('BugCatcher April','Insetos!! Insetos!! Procuro apenas pokemons Insetos!!', 'bugcatcher', 0);
+('Mãe', 'Oh, meu filho, cresceu tanto! Já vai sair na sua primeira jornada Pokémon... Não esqueça de me ligar S2', 'matriarca', 2),
+('Professor Oak', 'Olá treinador, sou o Professor Oak!! Você sabia que existem vários tipos de pokébola? Cada uma delas possui uma taxa de captura maior. Use elas com sabedoria', 'professor', 4),
+('Vendedor June', 'Bem vindo a minha lojinha! Fique avontade!!', 'vendedor', 6),
+('BugCatcher April', 'Insetos!! Insetos!! Procuro apenas pokemons Insetos!!', 'bugcatcher', 17),
+('Professor Algo', 'Tudo bem treinador? Me chamo Professor Algo. Você sabia que certos pokemons podem evoluir sem adquirir experiência? Basta dar a ele a Evostone correta!!', 'professor', 22),
+('Vendedora May', 'Ei muleque, não toque em nada que não vá pagar!!', 'vendedor', 26),
+('Aventureiro Jonas', 'Fazem alguns meses desde que sai da minha cidade natal para minha jornada, é um pouco solitário mas é uma experiência incrível', 'aventureiro', 43);
 
 -- Mochila
 INSERT INTO mochila (id, capacidade, dinheiro_maximo) VALUES ('Ash Ketchum', 50, 500.00);
